cf5057c334214cb708500370e4e149bf1a890df3
ErilMChen <53426685+ErilMChen@users.noreply.github.com>
2021-07-06 12:12:01 +0100

    Add files via upload
    favorite stop_name send to server

--- map/views.py
+++ map/views.py
@@ -5,14 +5,10 @@ from django.core import serializers
 from .models import *
 import json
 
-import users.views as uv
-import users.forms as au
-
 # Create your views here.
 def index(request):
-	form1 = au.UserForm()
-	form2 = au.AuthForm()
-	return render(request, 'index.html', {"form1": form1, "form2": form2})
+    return render(request, 'index.html')
+
 def BusStation(request):
 	ret = BusStops.objects.values('stop_name','stop_lat','stop_long').distinct()
 	data = list(ret)
@@ -22,18 +18,11 @@ def BusStation(request):
 def RouteDirection(request):
 	start_stop = request.GET.get("start_stop","")
 	end_stop = request.GET.get("end_stop","")
+	date = request.GET.get("date","")
+	time = request.GET.get("time","")
 	ret_start_stop = BusStops.objects.values('stop_name','stop_lat','stop_long').filter(stop_name = start_stop).distinct()
 	ret_end_stop = BusStops.objects.values('stop_name','stop_lat','stop_long').filter(stop_name = end_stop).distinct()
 	ret = chain(ret_start_stop, ret_end_stop)
 	data = list(ret)
 	data = json.dumps(data)
 	return HttpResponse(data)
-
-def login(response):
-    uv.login(response)
-
-def users(response):
-    uv.users(response)
-
-def extra(response):
-    uv.extra(response)
